6af62520105c1217f14f7f92441e75c2112a1146
Akash <akash300383@gmail.com>
2025-12-06 20:32:00 +0530

    done

--- FSD MERN - Foundations of Front-end Development/Programs/JavaScript ES5 and ES6/typesOfFunctions.js
+++ FSD MERN - Foundations of Front-end Development/Programs/JavaScript ES5 and ES6/typesOfFunctions.js
@@ -1,44 +1,57 @@
-// normal function - ES5 
-display1();     // we can call before declaration 
-function display1() {
-    console.log("normal function - display1 ")
-}
-display1();
+// // normal function - ES5 
+// display1();     // we can call before declaration 
+// function display1() {
+//     console.log("normal function - display1 ")
+// }
+// display1();
+// // expression style function - > ES5 
+// //display3();         // error 
+// let display3 = function() {
+//     console.log("expression style function - display1 ")
+// }
+// display3();
+// // arrow function --> es6 
+// let display4 = ()=>console.log("Arrow function")
+// display4();
+// // addition of two number using expression style function 
+// let addNumber1 = function(a,b){
+//     var sum = a+b;
+//     return sum;
+// }
+// console.log("Addition of two number using expression style "+addNumber1(10,20))
+// // addition of two number using arrow 
+// // arrow function by default return value without return keyword. 
+// let addNumber2 = (a,b)=>a+b;
+// console.log("Addition of two number using arrow style "+addNumber2(10,20))
+// // find largest of two number using expression style 
+// let findLargest1 = function(a,b){
+//     if(a>b){
+//         return "1st number largest"
+//     }else {
+//         return "2nd number largest"
+//     }
+// }
+// console.log("largest of two number is : "+findLargest1(100,50))
+// // find largest of two number using arrow style 
+// let findLargest2 = (a,b)=>{
+//     if(a>b){
+//         return "1st number largest"
+//     }else {
+//         return "2nd number largest"
+//     }
+// }
 
-// expression style function - > ES5 
-//display3();         // error 
-let display3 = function() {
-    console.log("expression style function - display1 ")
+// callback function 
+function greeting(fname,callback){
+    return "Welcome "+callback(fname);
 }
-display3();
-// arrow function --> es6 
-let display4 = ()=>console.log("Arrow function")
-display4();
-// addition of two number using expression style function 
-let addNumber1 = function(a,b){
-    var sum = a+b;
-    return sum;
+let maleInfo = function(fname){
+    return "Mr "+fname;
 }
-console.log("Addition of two number using expression style "+addNumber1(10,20))
-// addition of two number using arrow 
-// arrow function by default return value without return keyword. 
-let addNumber2 = (a,b)=>a+b;
-console.log("Addition of two number using arrow style "+addNumber2(10,20))
-// find largest of two number using expression style 
-let findLargest1 = function(a,b){
-    if(a>b){
-        return "1st number largest"
-    }else {
-        return "2nd number largest"
-    }
-}
-console.log("largest of two number is : "+findLargest1(100,50))
-// find largest of two number using arrow style 
-let findLargest2 = (a,b)=>{
-    if(a>b){
-        return "1st number largest"
-    }else {
-        return "2nd number largest"
-    }
-}
-
+let femaleInfo = (fname)=>"Miss "+fname;
+console.log(greeting("John",maleInfo))
+console.log(greeting("Neena",femaleInfo))
+console.log(greeting("Raj",function(fname){
+    return "Mr "+fname;
+}))
+console.log(greeting("Reeta",(fname)=>"Miss "+fname))
