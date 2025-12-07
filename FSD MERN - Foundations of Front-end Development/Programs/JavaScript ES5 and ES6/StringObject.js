a182e8810f8d0063236a86044f31e199c14b81b8
Akash <akash300383@gmail.com>
2025-12-07 20:11:05 +0530

    done

---
+++ FSD MERN - Foundations of Front-end Development/Programs/JavaScript ES5 and ES6/StringObject.js
@@ -0,0 +1,17 @@
+let msg1 = 'Welcome to JavaScript';
+let msg2 = "Welcome to JavaScript";
+let msg3 =  new String("Welcome to JavaScript");
+let msg4  ="Welcome" 
+    +"to"+ 
+    "JavaScript"; 
+let msg5 =`Welcome 
+        to 
+        JavaScript`
+console.log(msg1)
+console.log(msg4)
+console.log(msg5)
+console.log(msg1.length)
+console.log(msg1.toLowerCase())
+console.log(msg1.toUpperCase())
+console.log(msg1.substring(4))
+console.log(msg1.substring(4,10))
